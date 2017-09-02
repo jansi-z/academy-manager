dee3472d371a88a5580e01b2af38e6d5cf5e120d
Jan-Sijmen <js.zwarts@gmail.com>
2017-09-02 13:01:17 +0200

    added create for students

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,4 +6,23 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
   end
+
+  def new
+    @student = Student.new
+  end
+
+  def student_params
+    params.require(:student).permit(:name, :age, :stress_factor, :image_url, :teacher_id)
+  end
+
+  def create
+    @student = Student.new(student_params)
+
+    if @student.save
+      redirect_to @student
+    else
+      render "new"
+    end
+  end
+
 end
