b32812e4b32785748e0771895683862add450198
almozaz <alacruzmozaz@gmail.com>
2017-09-02 14:05:25 +0200

    Added delete student

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -39,4 +39,12 @@ class StudentsController < ApplicationController
     end
   end
 
+  def destroy
+    @student = Student.find(params[:id])
+
+    @student.destroy
+
+    redirect_to students_path
+  end
+
 end
